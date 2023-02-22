1f787466279705bbed8996fd1b1330c92112f70a
Silvia.A.A <silvia.alvarez.allende@se.experis.com>
2023-02-22 16:13:04 +0100

    Add: DDD

--- src/main/java/com/example/demo/Customer.java
+++ src/main/java/com/example/demo/Customer.java
@@ -3,12 +3,12 @@ package com.example.demo;
 public class Customer {
 
     int customer_id;
-    public String first_name;
-    String last_name;
-    String country;
-    String postal_code;
-    String phone;
-    String email;
+     public String first_name;
+    public String last_name;
+    public String country;
+    public String postal_code;
+    public String phone;
+    public String email;
 
     @Override
     public String toString() {
@@ -33,4 +33,12 @@ public class Customer {
         this.email = email;
     }
 
+    public Customer(String first_name, String last_name, String country, String postal_code, String phone, String email) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.country = country;
+        this.postal_code = postal_code;
+        this.phone = phone;
+        this.email = email;
+    }
 }
